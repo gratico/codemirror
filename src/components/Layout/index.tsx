b32c4259943b7545e91d315597399e123a9272c5
Abhishiv Saxena <abhishiv@gmail.com>
2023-07-08 10:16:50 +0530

    rf

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,23 +5,43 @@ import styles from "./Layout.module.scss";
 import { EditorView, basicSetup } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
-const setupCodemirror = (el: HTMLElement) => {
+const setupCodemirror = (el: HTMLIFrameElement) => {
+  console.log(el);
+  console.log(el.contentDocument, el.contentDocument?.body);
+  (el.contentDocument as Document).body.innerHTML = `<style>
+      html, body { margin: 0; padding: 0; height: 100vh; }
+      #codemirrorContainer {
+        height: 100vh;
+      }
+      #codemirrorContainer > div {
+        height: 100vh;
+      }
+      </style>
+      <div id="codemirrorContainer"></div>
+      `;
   let editor = new EditorView({
     extensions: [basicSetup, javascript()],
-    parent: el,
+    parent: el.contentDocument?.querySelector("#codemirrorContainer")!,
   });
+  return editor;
 };
 
 export const Layout = component("Layout", (props, utils) => {
   let container: HTMLElement | undefined;
+  let editor: EditorView | undefined;
   return (
-    <div
+    <iframe
+      frameBorder={"none"}
       ref={(el) => {
         container = el;
-        setupCodemirror(el);
+      }}
+      onLoad={() => {
+        if (container) {
+          editor = setupCodemirror(container as HTMLIFrameElement);
+        }
       }}
       class={styles.container}
-    ></div>
+    ></iframe>
   );
 });
 
